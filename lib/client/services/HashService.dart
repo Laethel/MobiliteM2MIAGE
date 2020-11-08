666eb7ba772ec64ed69aa04feaebe2ca6d4caf87
Andréa CHRISTOPHE <developper.andrea.christophe@gmail.com>
2020-11-08 13:47:10 +0100

    Auth Implementation
    Implementation of Google authentification and Mail/Password authentification + FireStore POC

---
+++ lib/client/services/HashService.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:common_utils/common_utils.dart';
+import 'package:crypto/crypto.dart';
+
+class HashService {
+
+  static encode(String toEncode) {
+
+    var bytes = utf8.encode(toEncode);
+    var digest = sha256.convert(bytes);
+
+    print("Digest as bytes: ${digest.bytes}");
+    print("Digest as hex string: $digest");
+
+  }
+
+}
